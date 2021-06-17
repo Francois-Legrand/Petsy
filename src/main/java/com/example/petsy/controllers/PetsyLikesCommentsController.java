0461e0e8ac61b1642205de35249719a18ed3c4b1
Gwen <dryystan@outlook.fr>
2021-06-17 16:40:35 +0200

    fin PetsyLikesComments, à tester

--- src/main/java/com/example/petsy/controllers/PetsyLikesCommentsController.java
+++ src/main/java/com/example/petsy/controllers/PetsyLikesCommentsController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,23 @@ public class PetsyLikesCommentsController {
 		return this.service.findAll();
 	}
 	
+	@GetMapping("{id}")
+	public PetsyLikesCommentsDTO findById(@PathVariable String id) {
+		return this.service.findById(id);
+	}
 	
+	@PutMapping("")
+	public PetsyLikesCommentsDTO edit(@RequestBody PetsyLikesCommentsDTO petsyLikesCommentsDTO) {
+		return this.service.save(petsyLikesCommentsDTO);
+	}
+	
+	@PostMapping("")
+	public PetsyLikesCommentsDTO save(@RequestBody PetsyLikesCommentsDTO petsyLikesCommentsDTO) {
+		return this.service.save(petsyLikesCommentsDTO);
+	}
+	
+	@DeleteMapping("{id}")
+	public void deleteById(@PathVariable String id) {
+		this.service.deleteById(id);
+	}
 }
