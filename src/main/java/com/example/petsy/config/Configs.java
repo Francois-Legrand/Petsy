a7b9b30802a83f141150d9a7dbb1c7a94327d932
Morgan Loose <VISION.m2i.wetic@gmail.com>
2021-06-17 15:29:47 +0200

    updated config with Pet bean

--- src/main/java/com/example/petsy/config/Configs.java
+++ src/main/java/com/example/petsy/config/Configs.java
@@ -2,6 +2,8 @@ package com.example.petsy.config;
 
 import com.example.petsy.repositories.IPhotoRepository;
 import com.example.petsy.repositories.IUserRepository;
+import com.example.petsy.repositories.PetRepository;
+import com.example.petsy.services.imp.PetServiceImpl;
 import com.example.petsy.services.imp.PhotoServiceImpl;
 import com.example.petsy.services.imp.UserServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,12 +12,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configs {
-    @Bean
-    public PhotoServiceImpl photoService(IPhotoRepository photoRepository, ObjectMapper mapper){
-        return new PhotoServiceImpl(photoRepository, mapper);
-    }
-    @Bean
-    UserServiceImpl userService(IUserRepository userRepository, ObjectMapper mapper){
-        return new UserServiceImpl(userRepository, mapper);
-    }
+	@Bean
+	public PhotoServiceImpl photoService(IPhotoRepository photoRepository, ObjectMapper mapper) {
+		return new PhotoServiceImpl(photoRepository, mapper);
+	}
+
+	@Bean
+	UserServiceImpl userService(IUserRepository userRepository, ObjectMapper mapper) {
+		return new UserServiceImpl(userRepository, mapper);
+	}
+
+	@Bean
+	PetServiceImpl petService(PetRepository PetRepository, ObjectMapper mapper) {
+		return new PetServiceImpl(PetRepository, mapper);
+	}
 }
