a8faaf3315690e0e957cbfa2b1cef72aac18d6f8
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-06-17 15:13:11 +0200

    add photo service imp repo config(moi et shahin) controller

---
+++ src/main/java/com/example/petsy/config/Configs.java
@@ -0,0 +1,21 @@
+package com.example.petsy.config;
+
+import com.example.petsy.repositories.IPhotoRepository;
+import com.example.petsy.repositories.IUserRepository;
+import com.example.petsy.services.imp.PhotoServiceImpl;
+import com.example.petsy.services.imp.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configs {
+    @Bean
+    public PhotoServiceImpl photoService(IPhotoRepository photoRepository, ObjectMapper mapper){
+        return new PhotoServiceImpl(photoRepository, mapper);
+    }
+    @Bean
+    UserServiceImpl userService(IUserRepository userRepository, ObjectMapper mapper){
+        return new UserServiceImpl(userRepository, mapper);
+    }
+}
