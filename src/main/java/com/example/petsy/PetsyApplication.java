38ebe2cddb882bd4b0984bb47883fd8d161e67c5
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-06-17 10:19:45 +0200

    first commit

---
+++ src/main/java/com/example/petsy/PetsyApplication.java
@@ -0,0 +1,13 @@
+package com.example.petsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petsy/PetsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
