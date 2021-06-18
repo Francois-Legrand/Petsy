a57440b1f7d8e6fef4bb1426f17137ae0f689a36
amandinecleret <taralya63@hotmail.fr>
2021-06-18 11:50:34 +0200

    rajout likecomment

--- src/main/java/com/example/petsy/controllers/PetsyController.java
+++ src/main/java/com/example/petsy/controllers/PetsyController.java
@@ -1,22 +1,19 @@
 package com.example.petsy.controllers;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.petsy.dto.PetsyDto;
-import com.example.petsy.models.Petsy;
-import com.example.petsy.services.IPetsyService;
 import com.example.petsy.services.imp.PetsyServiceImpl;
 
 @RestController
--- src/main/java/com/example/petsy/repositories/PetsyRepository.java
+++ src/main/java/com/example/petsy/repositories/PetsyRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.petsy.models.Petsy;
 
+
 import java.util.List;
 
 public interface PetsyRepository extends MongoRepository<Petsy, String> {
 	List<Petsy> findByContenu(String contenu);
 	List<Petsy> findByPersonne(Personne personne);
+	
 }
 
